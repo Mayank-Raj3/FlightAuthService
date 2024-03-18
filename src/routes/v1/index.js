aa002b26365674f7310b99f5f5838289ea14d785
Mayank-Raj3 <guptamannu884@gmail.com>
2024-03-18 18:02:50 +0530

    Added roles userroles through table and seeded data in roles

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -18,10 +18,10 @@ router.post(
 
 router.get("/isAuthenticated", UserController.isAuthenticated);
 
-// router.get(
-//   "/isAdmin",
-//   AuthRequestValidators.validateIsAdminRequest,
-//   UserController.isAdmin
-// );
+router.get(
+  "/isAdmin",
+  AuthRequestValidators.validateIsAdminRequest,
+  UserController.isAdmin
+);
 
 module.exports = router;
