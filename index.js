077a4f385d654b0c17e4d6c55af0f36fc515038f
Mayank-Raj3 <guptamannu884@gmail.com>
2024-03-18 16:17:36 +0530

    Added signin signup generation of jwt token Middlewares for email pass not present

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require("./src/config/server-config");
+const apiRoutes = require("./src/routes/index");
 
 const app = express();
+
 const prepareAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
   app.listen(PORT, async () => {
     console.log(`Server Started on Port: ${PORT}`);
   });
